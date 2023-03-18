fdf16583dc86542eb553ffeab7cd3d0900831b79
KamrulHasan <kamrulhasan3288@gmail.com>
2023-03-18 21:25:28 +0600

    implement string example and documentation

---
+++ 3_string_example.py
@@ -0,0 +1,18 @@
+# create a string using double or single quotes
+firstName = "Kamrul"
+lastName = "Hasan"
+print(firstName)
+
+print(firstName.upper())
+print(firstName.lower())
+
+# Format string
+print("{}\t{}".format(firstName, lastName))
+
+
+# Access String Characters in Python
+print(f"First index the of {firstName} is: {firstName[0]}")
+print(f"last index the of {firstName} is: {firstName[-1]}")
+
+# Slicing
+print(f"{firstName} name of 1st index to 4 index: {firstName[1:4]}")
