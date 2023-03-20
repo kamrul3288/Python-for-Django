1a8367cba18e7e2b1cbcb5383d551cc4ca03fc36
KamrulHasan <kamrulhasan3288@gmail.com>
2023-03-21 00:41:21 +0600

    implement if-statement example and documentation

---
+++ 5_if_statement_bootcamp.py
@@ -0,0 +1,16 @@
+cars = ['audi', 'bmw', 'subaru', 'toyota']
+
+for car in cars:
+    if car == "bmw":
+        print("BMW car found")
+
+# Person age prediction
+age = 50
+if age < 10:
+    print("You are a kid")
+elif age < 20:
+    print("You are teen age")
+elif age < 40:
+    print("You are young")
+else:
+    print("You are old")
