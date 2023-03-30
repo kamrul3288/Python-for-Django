a1e70ff5bffcd82395c6c01666111289357dc518
KamrulHasan <kamrulhasan3288@gmail.com>
2023-03-31 01:58:17 +0600

    implement for loop example and documentation

---
+++ 12_for_loop_bootcamp.py
@@ -0,0 +1,17 @@
+
+
+for x in range(3):
+    print(x)
+
+for x in range(8, 10):
+    print(x)
+
+cars = ["BMW", "Audi", "Honda"]
+for car in cars:
+    print(car)
+
+
+for x in range(15, 20):
+    print(x)
+else:
+    print("Loop end")
